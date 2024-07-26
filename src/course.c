1d2c0ad5160a3a8321a9734c10919cc84efe7220
sidhartha235 <ananthulasidhartha03@gmail.com>
2024-07-26 23:29:07 +0530

    AddCourse functionality

--- src/course.c
+++ src/course.c
@@ -4,18 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int addStudentCourse (int rollNumber, int courseCode, int marks) {
-
-}
-
-int modifyStudentCourse (int rollNumber, int courseCode, int marks) {
-
-}
-
-int deleteStudentCourse (int rollNumber, int courseCode) {
-
-}
-
 CourseNode* searchStudentCourse (CourseNode* courseHead, int courseCode) {
     CourseNode* temporaryCourse = courseHead;
     while (temporaryCourse != NULL) {
@@ -25,4 +13,49 @@ CourseNode* searchStudentCourse (CourseNode* courseHead, int courseCode) {
         temporaryCourse = temporaryCourse->nextCourse;
     }
     return NULL;
+}
+
+int addStudentCourse (int rollNumber, int courseCode, int marks) {
+    StudentNode* studentNode = searchStudent(rollNumber);
+    if (studentNode == NULL) {
+        printf("Add Course: Student does not exist.\n", rollNumber); // change to file logs later
+        return 0;
+    } else {
+        CourseNode* newCourse = searchStudentCourse(studentNode->student.courseHead, courseCode);
+        if (newCourse != NULL) {
+            printf("Add: Course %d already exists!\n", courseCode); //change to file logs later
+            return 0;
+        } else {
+            newCourse = (CourseNode*) malloc(sizeof(CourseNode));
+            if (newCourse == NULL) {
+                printf("Failed to allocate memory while adding new course!\n");
+                exit(1);
+            }
+
+            newCourse->course.courseCode = courseCode;
+            newCourse->course.marks = marks;
+
+            newCourse->nextCourse = NULL;
+            newCourse->previousCourse = NULL;
+
+            if (studentNode->student.courseHead != NULL) {
+                newCourse->nextCourse = studentNode->student.courseHead;
+                studentNode->student.courseHead->previousCourse = newCourse;
+                studentNode->student.courseHead = newCourse;
+            } else {
+                studentNode->student.courseHead = newCourse;
+            }
+
+            printf("Success: %d added for %d.\n", courseCode, rollNumber);
+            return 1;
+        }
+    }
+}
+
+int modifyStudentCourse (int rollNumber, int courseCode, int marks) {
+
+}
+
+int deleteStudentCourse (int rollNumber, int courseCode) {
+
 }
